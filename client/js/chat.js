604df20f613d1e64d726d846a95a3a4061184d60
Szymon Grobelny <sgrobelny.contact@gmail.com>
2021-12-29 15:15:03 +0100

    Test public handle API

--- client/js/chat.js
+++ client/js/chat.js
@@ -5,27 +5,27 @@ const chatView = document.querySelector("#Chat");
 // TO DO Handle API
 function getChat() {
     const currentUsername = localStorage.getItem("username");
-    fetch('http://localhost:63342/DH-keys-distribution/client/test/chat.json', {
+    fetch('https://localhost:44310/api/chat', {
         method: 'GET'
     })
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            data.forEach(message => {
-                let messageClass;
-                if (message.author === currentUsername) {
-                    messageClass = "message message--own";
-                } else {
-                    messageClass = "message message--other";
-                }
-                let messageHTML =
-                    `<div class="${messageClass}">
-                    <div class="message__Author">${message.author}</div>
-                    <div class="message__Value">${message.text}</div>
-                    <div class="message__Time">${message.time}</div>
-                </div>`;
-                chatView.insertAdjacentHTML("beforeend", messageHTML);
-            })
+            // data.forEach(message => {
+            //     let messageClass;
+            //     if (message.author === currentUsername) {
+            //         messageClass = "message message--own";
+            //     } else {
+            //         messageClass = "message message--other";
+            //     }
+            //     let messageHTML =
+            //         `<div class="${messageClass}">
+            //         <div class="message__Author">${message.author}</div>
+            //         <div class="message__Value">${message.text}</div>
+            //         <div class="message__Time">${message.time}</div>
+            //     </div>`;
+            //     chatView.insertAdjacentHTML("beforeend", messageHTML);
+            // })
         });
 }
 
